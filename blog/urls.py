10b3a1aa8922511dc0dcf34cc44af7954fba7bc0
djcg <daniel_gilliam@hotmail.com>
2019-05-14 20:46:36 +0100

    Basic front end html to display list of blog posts and blog posts

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('blog/<int:pk>/', views.DetailView.as_view(), name='detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from blog.models import Post
+
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_posts'
+
+    def get_queryset(self):
+	    '''Return last five blog posts'''
+	    return Post.objects.order_by('-published_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Post
+	template_name = 'blog/detail.html'
