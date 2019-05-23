ec4e9eece30c5f4d19bc49ad80ba5e309d243f07
djcg <daniel_gilliam@hotmail.com>
2019-05-23 21:01:41 +0100

    Stylesheet on post detail page and url correction

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from blog import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('blog/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
