2b6228d1b2edfcd9f4358d16dc0ed7665ed0c54b
djcg <daniel_gilliam@hotmail.com>
2019-05-27 21:54:40 +0100

    Added models for post images
    *PostImage model to store images for each post
    *Added category model to enable categories to be attached to posts
    *Added issue model to allow posts to be part of different issues

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.conf import settings
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.utils import timezone
 
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    """Blog post model"""
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # TODO: Change to user model when user app implemented
+    issue = models.ForeignKey('Issue', on_delete=models.CASCADE, default=-1)
+    category = models.MantToManyField('Category', related_name='posts')
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
@@ -15,3 +20,31 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class PostImage(models.Model):
+    """Blog post images"""
+    post = models.ForeignKey(Post, default=None)
+    image = models.ImageField(upload_to=get_image_filename, verbose_name="Image")
+
+class Category(models.Model):
+    """Post categories"""
+    name = models.CharField(max_length=20)
+
+class Issue(models.Model):
+    """Table for zine issue"""
+    isuue_number = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return str(self.issue_number)
+
+def get_image_filename(instance, filename):
+    title = instance.post.title
+    slug = slugify(title)
+    return "images/%s-%s" % (slug, filename) 
