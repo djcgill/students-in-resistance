534230852d6abf88c89934b065560b3092a82588
djcg <daniel_gilliam@hotmail.com>
2020-06-10 20:03:37 +0100

    Added login/logout and template for registration. FInally figured out css and did button in top right

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from accounts import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
