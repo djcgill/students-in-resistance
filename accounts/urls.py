af083e5a24c4c8e97fe0afe5a75b7eaf645764af
djcg <daniel_gilliam@hotmail.com>
2020-06-20 15:26:38 +0100

    Added registration page to sign up with

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('registration/', views.register, name='registration'),
 ]
